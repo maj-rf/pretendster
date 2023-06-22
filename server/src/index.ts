be62ea8fc43cd8d3a827fbe84b51eb52e088c5d8
bananabread08 <rmkfermin@gmail.com>
2023-06-22 21:35:51 +0800

    server initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,8 @@
+import app from './app';
+import http from 'http';
+import { PORT } from './config/config';
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
