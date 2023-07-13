b591378770e2cee2bab13829f4fdff7882cedae1
bananabread08 <rmkfermin@gmail.com>
2023-07-13 10:51:22 +0800

    create auth context and hook

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useReducer } from 'react';
+
+interface PublicUser {
+  username: string;
+  email: string;
+  id: string;
+}
+
+type Action = { type: 'login'; payload: PublicUser } | { type: 'logout' };
+type Dispatch = (action: Action) => void;
+type State = { user: PublicUser | null };
+type AuthProviderProps = { children: React.ReactNode };
+
+const initialState: State = {
+  user: localStorage.getItem('user')
+    ? JSON.parse(localStorage.getItem('user') || '')
+    : null,
+};
+
+export const AuthContext = createContext<
+  { state: State; dispatch: Dispatch } | undefined
+>(undefined);
+
+function authReducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'login': {
+      localStorage.setItem('user', JSON.stringify(action.payload));
+      return { ...state, user: action.payload };
+    }
+    case 'logout': {
+      localStorage.removeItem('user');
+      return { ...state, user: null };
+    }
+    default: {
+      throw new Error('Unhandled action type');
+    }
+  }
+}
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+  const value = { state, dispatch };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+export { AuthProvider };
