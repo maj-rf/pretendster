78025a082689dadcb497cb8d32224f32a9cf9d01
bananabread08 <rmkfermin@gmail.com>
2023-09-22 10:34:09 +0800

    remove username from profile url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: '/profile/:id/:username',
+        path: '/profile/:id/',
         element: <Profile />,
       },
     ],
