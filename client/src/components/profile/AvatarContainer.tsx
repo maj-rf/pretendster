e8c9c77708a6695db222277191d8271ae2ef6d19
bananabread08 <rmkfermin@gmail.com>
2023-09-22 10:33:19 +0800

    add button to profile pic for img upload, update style

--- client/src/components/profile/AvatarContainer.tsx
+++ client/src/components/profile/AvatarContainer.tsx
@@ -11,7 +11,7 @@ import {
 import { useState } from 'react';
 import { ChangeProfilePicModal } from './ChangeProfilePicModal';
 import { Button } from '../ui/button';
-import { UserMinus, UserPlus } from 'lucide-react';
+import { Plus, UserMinus, UserPlus } from 'lucide-react';
 import { useAuth } from '@/hooks/useAuth';
 import { followUser, unfollowUser } from '@/services/userService';
 import { useQueryClient, useMutation } from '@tanstack/react-query';
@@ -46,16 +46,27 @@ export const AvatarContainer = ({ data }: { data: IUser }) => {
   return (
     <div className="container mx-auto flex items-center justify-between absolute bottom-[-1rem] left-1/2 transform -translate-x-1/2">
       <Dialog open={showPicModal} onOpenChange={setShowPicModal}>
-        <div className="flex items-center relative">
-          <DialogTrigger asChild>
-            <Avatar className="w-28 h-auto">
+        <div className="flex items-center">
+          <div className="group">
+            <Avatar className="w-28 h-auto border-4 relative">
               <AvatarImage
                 src={data.profileImg}
                 className="object-cover object-top"
               />
               <AvatarFallback>{data.username.slice(0, 2)}</AvatarFallback>
+              {state.user?.id === data.id ? (
+                <DialogTrigger asChild>
+                  <Button
+                    className="hidden group-hover:block absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full"
+                    variant="ghost"
+                  >
+                    <Plus className="w-full h-full" />
+                  </Button>
+                </DialogTrigger>
+              ) : null}
             </Avatar>
-          </DialogTrigger>
+          </div>
+
           <div className="flex items-center gap-4 translate-y-8">
             <div className="bg-secondary px-2 py-1 rounded-2xl font-medium border-2 border-primary">
               {data.username}
