bb3779eed12a225ac7151e0c468659d5ffe45b82
bananabread08 <rmkfermin@gmail.com>
2023-10-31 09:14:15 +0800

    install helmet, compression, and rateLimit

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,9 @@ import 'express-async-errors';
 import * as middleware from './middleware/middleware';
 import cors from 'cors';
 import morgan from 'morgan';
+import compression from 'compression';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import cookieParser from 'cookie-parser';
 import { authRouter } from './routes/auth';
 import { userRouter } from './routes/user';
@@ -10,11 +13,19 @@ import { postRouter } from './routes/post';
 import { commentRouter } from './routes/comment';
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
+app.use(limiter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World');
