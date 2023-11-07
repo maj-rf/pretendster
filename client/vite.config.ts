98351661884c3d57c8653c92f38457d499d7a3d5
bananabread08 <rmkfermin@gmail.com>
2023-11-07 20:14:29 +0800

    change client and server settings

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,22 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../server/dist',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'http://localhost:3006/api/v1',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api\/v1/, ''),
-      },
     },
   },
 });
